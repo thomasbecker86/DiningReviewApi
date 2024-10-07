15e746e80815f39ca51b1c973848adba902b2c1a
Thomas <thomasbecker86@gmx.de>
2024-10-07 19:55:40 +0200

    Started implementing ReviewController and ReviewRepository

--- src/main/java/com/example/diningReviewApi/controller/ReviewController.java
+++ src/main/java/com/example/diningReviewApi/controller/ReviewController.java
@@ -4,8 +4,20 @@
  */
 package com.example.diningReviewApi.controller;
 
+import com.example.diningReviewApi.model.DiningReview;
+import com.example.diningReviewApi.model.User;
+import com.example.diningReviewApi.repository.ReviewRepository;
+import com.example.diningReviewApi.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -13,6 +25,26 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @RestController
+@RequestMapping(path = "/reviews")
 public class ReviewController {
+    
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+
+    public ReviewController(ReviewRepository reviewRepository, UserRepository userRepository) {
+        this.reviewRepository = reviewRepository;
+        this.userRepository = userRepository;
+    }
+    
+    //TODO: Input-Validierung, HttpStatus
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public DiningReview addReview(@RequestBody DiningReview review, @PathVariable String userName) {
+        Optional<User> optionalUser = this.userRepository.findByName(userName);
+        if (optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You don't have permission to submit a review.");
+        }
+        return this.reviewRepository.save(review);
+    }
     
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Thomas
+ * Created: 07.10.2024
+ */
+
+insert into RESTAURANTS (id, name, city, street, zip_code, peanut_score, egg_score, dairy_score, total_score) values (001, 'Bareburger', 'Los Angeles', 'Main street 123', 12345, 5, 4, 3, 4);
+
